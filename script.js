fa66cca0ac3b28842b552c9e34b323950e24c20e
Pysia20 <pysiaczek642@gmail.com>
2026-08-02 13:27:02 +0200

    added some classes in css for later and reorganized it so it looks somewhat better, also started implementing the rng

---
+++ script.js
@@ -0,0 +1,21 @@
+let correct = ""
+
+function generate() {
+    const method = Math.floor(Math.random() * 4) //wait, it's all just math.random?  always has been.
+    switch (method) {
+        case 0:
+            break
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        default:
+            console.log("switch err")
+    }
+}
+
+function guess(anwser) {
+    
+}
